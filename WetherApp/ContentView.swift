3a05553b20f959d784f422556cbbeff5ddd3566a
blur1379 <blor1379@gmail.com>
2022-05-12 21:45:56 +0430

    Initial Commit

---
+++ WetherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WetherApp
+//
+//  Created by Blur on 2/22/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WetherApp/WetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherAppApp.swift
+//  WetherApp
+//
+//  Created by Blur on 2/22/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct WetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
